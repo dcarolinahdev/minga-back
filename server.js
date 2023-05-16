97a190770457d39a59c223c5c0f7348107307065
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2023-05-15 19:36:44 -0500

    using middlewares from express

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ import "dotenv/config.js"
 import "./config/database.js"
 
 const server = express(); // definición de servidor
-
 const PORT = process.env.PORT || 8080; // definición de puerto
-
 const ready = () => console.log('ready on port '+ PORT);
 
+// middlewares
+server.use(express.json()); // permite entradas y trabajar con con formato JSON
+server.use(express.urlencoded({ extended: false })); // permite capturar consultas complejas
+
+// router + server
 server.listen(PORT, ready); // iniciar servidor
 
 console.log(process.env)
