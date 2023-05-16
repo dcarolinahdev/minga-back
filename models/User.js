44ebf7f464897a96150072402c96a7793ee9bf5c
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-05-16 17:02:25 -0500

    Minga 03 - Model User (#3)
    * First model: user
    
    * Adding timestamps to model
    
    * First model with Express and mongo

---
+++ models/User.js
@@ -0,0 +1,16 @@
+import {Schema, model} from 'mongoose'
+
+let collection = 'users'
+
+let schema = new Schema ({
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    photo: {type: String, required: true},
+    role: {type: Number, required: true},
+    online: {type: Boolean, required: true}
+},{
+    timestamps: true
+})
+
+let User = model(collection, schema)
+export default User
---
+++ models/data/users.js
@@ -0,0 +1,55 @@
+import "dotenv/config.js"
+import "../../config/database.js"
+import User from "../User.js"
+
+let users = [{
+    email: "alejandro@mh.com.ar",
+    password: "$2a$10$XZBDuOYHcHSzZNzf2ws7Quvu3YmKgSagWRUG1eGlBLSdxJunpN1LC",
+    role: 1,
+    online: false,
+    photo: "https://i.postimg.cc/fyJsspq8/image.png"
+},{
+    email: "lucas@mh.com.ar",
+    password: "$2a$10$XZBDuOYHcHSzZNzf2ws7Quvu3YmKgSagWRUG1eGlBLSdxJunpN1LC",
+    role: 1,
+    online: false,
+    photo: "https://i.postimg.cc/fyJsspq8/image.png"
+},{
+    email: "jose@mh.com.ar",
+    password: "$2a$10$XZBDuOYHcHSzZNzf2ws7Quvu3YmKgSagWRUG1eGlBLSdxJunpN1LC",
+    role: 1,
+    online: false,
+    photo: "https://i.postimg.cc/GhdNvZxV/5831a17a290077c646a48c4db78a81bb-icono-de-perfil-de-usuario-azul.png"
+},{
+    email: "eric@mh.com.ar",
+    password: "$2a$10$XZBDuOYHcHSzZNzf2ws7Quvu3YmKgSagWRUG1eGlBLSdxJunpN1LC",
+    role: 1,
+    online: false,
+    photo: "https://i.postimg.cc/GhdNvZxV/5831a17a290077c646a48c4db78a81bb-icono-de-perfil-de-usuario-azul.png"
+},{
+    email: "igna@mh.com.ar",
+    password: "$2a$10$XZBDuOYHcHSzZNzf2ws7Quvu3YmKgSagWRUG1eGlBLSdxJunpN1LC",
+    role: 1,
+    online: false,
+    photo: "https://i.postimg.cc/7Yj2FytQ/43cc80b4c098e43a988c535eaba42c53-icono-de-usuario-de-persona.png"
+},{
+    email: "silvina@mh.com.ar",
+    password: "$2a$10$XZBDuOYHcHSzZNzf2ws7Quvu3YmKgSagWRUG1eGlBLSdxJunpN1LC",
+    role: 3,
+    online: false,
+    photo: "https://i.postimg.cc/7Yj2FytQ/43cc80b4c098e43a988c535eaba42c53-icono-de-usuario-de-persona.png"
+},{
+    email: "peace@team.com.ar",
+    password: "$2a$10$XZBDuOYHcHSzZNzf2ws7Quvu3YmKgSagWRUG1eGlBLSdxJunpN1LC",
+    role: 2,
+    online: false,
+    photo: "https://i.postimg.cc/2jZbntSQ/image.png"
+},{
+    email: "digital@team.com.ar",
+    password: "$2a$10$XZBDuOYHcHSzZNzf2ws7Quvu3YmKgSagWRUG1eGlBLSdxJunpN1LC",
+    role: 2,
+    online: false,
+    photo: "https://i.postimg.cc/2jZbntSQ/image.png"
+}]
+
+User.insertMany(users)
