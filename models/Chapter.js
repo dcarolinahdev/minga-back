139a41a4ba211f784ae268327ad1e0fcd03b293b
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-05-16 20:48:39 -0500

    minga-07 Definition Chapter model (#7)

---
+++ models/Chapter.js
@@ -0,0 +1,16 @@
+import {Schema, model, Types} from 'mongoose'
+
+let collection = 'chapters'
+
+let schema = new Schema ({
+    manga_id: {type: Types.ObjectId, ref:'mangas', required: true},
+    title: {type: String, required: true},
+    cover_photo: {type: String, required: true},
+    pages: {type: String, required: true},
+    order: {type: Number, required: true}
+},{
+    timestamps: true
+})
+
+let Chapter = model(collection, schema)
+export default Chapter
