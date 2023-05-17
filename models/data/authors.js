7d811e1e20ca1db05dc7ab243178e8d18ecb53d6
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-05-16 19:53:35 -0500

    Definition Authors model (#4)

---
+++ models/data/authors.js
@@ -0,0 +1,45 @@
+import "dotenv/config.js"
+import "../../config/database.js"
+import Author from "../Author.js"
+
+let authors = [{
+    name: "alejandro",
+    city: "buenos aires",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '6463fb800b365aca9ada778e'
+},{
+    name: "lucas",
+    last_name: "silva",
+    city: "buenos aires",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '6463fb800b365aca9ada778f'
+},{
+    name: "jose",
+    last_name: "lopez",
+    city: "villa carlos paz",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '6463fb800b365aca9ada7790'
+},{
+    name: "eric",
+    city: "resistencia",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '6463fb800b365aca9ada7791'
+},{
+    name: "igna",
+    last_name: "borraz",
+    city: "rosario",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true,
+    user_id: '6463fb800b365aca9ada7792'
+}]
+
+Author.insertMany(authors)
