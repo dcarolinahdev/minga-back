9678329b3f48714ed10ff08e9cb1d4c708fc0258
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2023-05-18 19:15:58 -0500

    script for chapters and mangas

--- models/Chapter.js
+++ models/Chapter.js
@@ -6,7 +6,7 @@ let schema = new Schema ({
     manga_id: {type: Types.ObjectId, ref:'mangas', required: true},
     title: {type: String, required: true},
     cover_photo: {type: String, required: true},
-    pages: {type: String, required: true},
+    pages: {type: Array, required: true},
     order: {type: Number, required: true}
 },{
     timestamps: true
--- models/data/chapters.js
+++
@@ -1,7 +0,0 @@
-import "dotenv/config.js"
-import "../../config/database.js"
-import Chapter from "../Chapter.js"
-
-let chapters = []
-
-Chapter.insertMany(chapters)
