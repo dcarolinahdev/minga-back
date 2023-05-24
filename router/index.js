13fb952c440d408e949041f88dd20a2c6926176c
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2023-05-23 21:00:48 -0500

    using routers with controllers - read mangas

--- router/index.js
+++ router/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import authorsRouter from "./authors.js"
+import mangasRouter from "./mangas.js"
 
 let indexRouter = Router()
 
 indexRouter.use('/authors', authorsRouter)
+indexRouter.use('/mangas', mangasRouter)
 
 export default indexRouter
